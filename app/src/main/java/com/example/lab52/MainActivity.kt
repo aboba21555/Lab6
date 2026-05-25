8d50cdaf4d009952928bc80e604ef88d8e840316
sohte2 <razedota19@gmail.com>
2026-05-25 19:55:36 +0300

    Break code for test

--- app/src/main/java/com/example/lab52/MainActivity.kt
+++ app/src/main/java/com/example/lab52/MainActivity.kt
@@ -29,7 +29,7 @@ class MainActivity : ComponentActivity() {
                 ) {
 
                     Greeting(
-                        name = "Берлодян Артур "
+                        name = "Берлодян Артур " помилка
                     )
 
                 }
